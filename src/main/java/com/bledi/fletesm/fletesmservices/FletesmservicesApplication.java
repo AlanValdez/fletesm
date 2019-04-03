c299a06ad7baa73f5d5196cc067dad14ef5a3a1b
alanb.valdez <alanb.valdez@softtek.com>
2019-04-03 12:42:25 -0600

    Primer commit, Crear proyecto Java

---
+++ src/main/java/com/bledi/fletesm/fletesmservices/FletesmservicesApplication.java
@@ -0,0 +1,13 @@
+package com.bledi.fletesm.fletesmservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FletesmservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FletesmservicesApplication.class, args);
+	}
+
+}
