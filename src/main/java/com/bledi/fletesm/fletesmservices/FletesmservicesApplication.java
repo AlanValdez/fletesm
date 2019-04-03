8c240aa09c4a4bf181ef013b0c655a9e02e33dfc
alanb.valdez <alanb.valdez@softtek.com>
2019-04-03 13:22:32 -0600

    Creamos modelo 'Flete'

--- src/main/java/com/bledi/fletesm/fletesmservices/FletesmservicesApplication.java
+++ src/main/java/com/bledi/fletesm/fletesmservices/FletesmservicesApplication.java
@@ -2,8 +2,10 @@ package com.bledi.fletesm.fletesmservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class FletesmservicesApplication {
 
 	public static void main(String[] args) {
