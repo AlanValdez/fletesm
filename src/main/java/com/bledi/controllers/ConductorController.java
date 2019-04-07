88d0c87ebb577e49d6309ddf9d598511cf8e5364
Alan Valdez <valdezalan97@outlook.com>
2019-04-06 22:25:47 -0600

    Added Interface for conductor controller

--- src/main/java/com/bledi/controllers/ConductorController.java
+++ src/main/java/com/bledi/controllers/ConductorController.java
@@ -15,36 +15,56 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.bledi.dao.ConductorDAO;
 import com.bledi.models.Conductor;
 @Controller
-public class ConductorController {
+public class ConductorController implements IConductorController {
 
 	@Autowired
 	  private ConductorDAO conductorDao;
 	
+	/* (non-Javadoc)
+	 * @see com.bledi.controllers.IConductorController#getAllConductores()
+	 */
+	@Override
 	@GetMapping("/conductores")
     @ResponseBody
     public ResponseEntity<List<Conductor>> getAllConductores() {
 		return new ResponseEntity<List<Conductor>>(conductorDao.getAllConductors(), HttpStatus.OK);   
     }
 	
+	/* (non-Javadoc)
+	 * @see com.bledi.controllers.IConductorController#createConductor(com.bledi.models.Conductor)
+	 */
+	@Override
 	@PostMapping("/conductores/create")
     @ResponseBody
     public ResponseEntity<Integer> createConductor(@RequestBody Conductor conductor) {
 		return new ResponseEntity<Integer>(conductorDao.createConductor(conductor), HttpStatus.OK);   
     }
 	
+	/* (non-Javadoc)
+	 * @see com.bledi.controllers.IConductorController#updateConductor(com.bledi.models.Conductor, int)
+	 */
+	@Override
 	@PostMapping("/conductores/update")
     @ResponseBody
     public ResponseEntity<Integer> updateConductor(@RequestBody Conductor conductor, @RequestParam(name="id", required=true) int conductor_id) {
 		return new ResponseEntity<Integer>(conductorDao.updateConductor(conductor, conductor_id), HttpStatus.OK);   
     }
 	
+	/* (non-Javadoc)
+	 * @see com.bledi.controllers.IConductorController#getAllConductores(java.util.List)
+	 */
+	@Override
 	@GetMapping("/conductores_list")
     @ResponseBody
     public ResponseEntity<List<Conductor>> getAllConductores(@RequestParam(name="ids",required = false) List<Integer> id_list) {
 		return new ResponseEntity<List<Conductor>>(conductorDao.getAllConductors(id_list), HttpStatus.OK);   
     }
     
-    @GetMapping("/conductores/by")
+    /* (non-Javadoc)
+	 * @see com.bledi.controllers.IConductorController#getConductorByID(int)
+	 */
+    @Override
+	@GetMapping("/conductores/by")
     @ResponseBody
     public ResponseEntity<Conductor> getConductorByID(@RequestParam(name="id", required=true) int conductor_id) {    
     	
