c8eb39e88f21f50e19609cc85e6c7f6f55b3ca34
Alan Valdez <valdezalan97@outlook.com>
2019-04-09 22:13:44 -0500

    Added connection to database

--- src/main/java/com/bledi/Main.java
+++ src/main/java/com/bledi/Main.java
@@ -49,8 +49,16 @@ public class Main {
 
   @Bean
   public DataSource dataSource() throws SQLException {
-    if (dbUrl == null || dbUrl.isEmpty()) {
-      return new HikariDataSource();
+	  String username = System.getenv("username");
+	  String pwf = System.getenv("password");
+	  String db = System.getenv("JAWSDB_URL");
+	  String env = System.getenv("enviroment");
+    if (dbUrl == null || dbUrl.isEmpty() || db != "" || env != "prod") {
+    	HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(db);
+        config.setUsername( username );
+        config.setPassword( pwf );
+        return new HikariDataSource(config);
     } else {
       HikariConfig config = new HikariConfig();
       config.setJdbcUrl(dbUrl);
