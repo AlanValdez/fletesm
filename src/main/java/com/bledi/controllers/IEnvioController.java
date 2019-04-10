2e01f4b68d1a867d926c710e70184e594e68dc2f
Alan Valdez <valdezalan97@outlook.com>
2019-04-09 21:20:53 -0500

    Created Envio controller

---
+++ src/main/java/com/bledi/controllers/IEnvioController.java
@@ -0,0 +1,21 @@
+package com.bledi.controllers;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+
+import com.bledi.models.Envio;
+
+public interface IEnvioController {
+
+	ResponseEntity<List<Envio>> getAllEnvio();
+
+	ResponseEntity<Integer> createEnvio(Envio envio);
+
+	ResponseEntity<Envio> getEnviosByConductorId(int conductor_id);
+	
+	ResponseEntity<Envio> getEnviosByTrailerId(int trailer_id);
+
+	ResponseEntity<Integer> cerrarEnvio(int conductor_id, int trailer_id);
+	
+}
