9d3abecdaf8e4bc7fe394538e412b637212d9355
Alan Valdez <valdezalan97@outlook.com>
2019-05-05 14:34:56 -0500

    Updated Envio model

--- src/main/java/com/bledi/models/Envio.java
+++ src/main/java/com/bledi/models/Envio.java
@@ -12,23 +12,45 @@ public class Envio {
 	private String estado;
 	private String ciudad;
 	private String direccion;
+	public String getNombreCompleto() {
+		return NombreCompleto;
+	}
+
+	public void setNombreCompleto(String nombreCompleto) {
+		NombreCompleto = nombreCompleto;
+	}
+
+	public String getPlacas() {
+		return placas;
+	}
+
+	public void setPlacas(String placas) {
+		this.placas = placas;
+	}
 	private String estado_actual;
+	private String NombreCompleto;
+	private String placas;
+
 	
 	public Envio() { }
 	
-	public Envio(int conductor_id, int trailer_id, String tipo_carga, String cliente, String estado_actual,
-			String estado, String ciudad, String direccion, Date fecha_salida) {
+
+	public Envio(int conductor_id, int trailer_id, String tipo_carga, Date fecha_salida, String cliente, String estado,
+			String ciudad, String direccion, String estado_actual, String nombreCompleto, String placas) {
 		super();
 		this.conductor_id = conductor_id;
 		this.trailer_id = trailer_id;
 		this.tipo_carga = tipo_carga;
+		this.fecha_salida = fecha_salida;
 		this.cliente = cliente;
-		this.estado_actual = estado_actual;
 		this.estado = estado;
 		this.ciudad = ciudad;
 		this.direccion = direccion;
-		this.fecha_salida = fecha_salida;
+		this.estado_actual = estado_actual;
+		NombreCompleto = nombreCompleto;
+		this.placas = placas;
 	}
+
 	public int getConductor_id() {
 		return conductor_id;
 	}
